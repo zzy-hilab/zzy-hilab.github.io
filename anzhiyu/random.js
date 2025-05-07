5fcec15d0c021e10f3f93221e9698bbc3fc22f77
zzy-hilab <zzypr2565310563@163.com>
2025-05-07 17:29:11 +0800

    Site updated: 2025-05-07 17:29:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/07/Hello-World-0/","2025/05/07/hello-world/"];function toRandomPost(){
+var posts=["2025/05/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
